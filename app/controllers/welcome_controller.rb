dc4c5ef7c4b33501a21c9918cd0bc87dfee024f4
Lawrence Abaeo <labaeo@gmail.com>
2014-09-24 11:07:58 -0700

    Bootstrap style some of the body.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @placeholder_url = "http://placehold.it/200x200"
+    @hero_placeholder_url = "http://placehold.it/300x350"
   end
 end
