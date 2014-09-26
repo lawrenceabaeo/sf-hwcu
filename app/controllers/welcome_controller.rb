e89986bd7873c9504b431fc8f8bc866d3b4b18a5
Lawrence Abaeo <labaeo@gmail.com>
2014-09-26 11:35:21 -0700

    Add real district data instead of placeholder data for some parts of welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @placeholder_url = "http://placehold.it/200x200"
     @hero_placeholder_url = "http://placehold.it/300x350"
+    @districts = District.order(district_number: :asc)
   end
 end
