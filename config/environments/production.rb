0ad0718e1face89914306c28f00d9945b6ce670f
Lawrence Abaeo <labaeo@gmail.com>
2014-09-27 20:35:38 -0700

    Change a production config setting to see if it fixes missing images in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ SfHwcu::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+  config.assets.compile = true # My images are 404 on heroku, stackoverflow says this fixes it
 
   # Generate digests for assets URLs.
   config.assets.digest = true
