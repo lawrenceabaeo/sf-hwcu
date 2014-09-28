18b93e73a347cee1ad0b812ee9d7b996b3fb2531
Lawrence Abaeo <labaeo@gmail.com>
2014-09-28 15:32:58 -0700

    Update main copy on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @placeholder_url = "http://placehold.it/200x200"
-    @hero_placeholder_url = "http://placehold.it/300x350"
+    @hero_image = "hwcu-image_600x700.png"
     @districts = District.order(district_number: :asc)
+    @resources_url = "#"
   end
 end
