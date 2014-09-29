5d632312746a412a5ce8c66e9f3d0b2644c7bff1
Lawrence Abaeo <labaeo@gmail.com>
2014-09-29 16:35:49 -0700

    Add city seal. Tweak hazmat logo. Tweak citation links.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @all_districts_image = "San_Francisco_District_ALL_(2012).svg"
-    @hero_image = "hwcu-image_600x700.png"
+    @hero_image = "hazmat-logo-001.png"
     @districts = District.order(district_number: :asc)
     @contact_url = "#Contact"
+    @sf_seal_image = "sf_seal.png"
   end
 end
