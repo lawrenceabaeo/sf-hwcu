44a09a361c988086ec78502d89f30fd6548d4e5b
Lawrence Abaeo <labaeo@gmail.com>
2014-09-29 11:32:33 -0700

    Add image for all districts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @placeholder_url = "http://placehold.it/200x200"
+    @all_districts_image = "San_Francisco_District_ALL_(2012).svg"
     @hero_image = "hwcu-image_600x700.png"
     @districts = District.order(district_number: :asc)
     @resources_url = "#"
